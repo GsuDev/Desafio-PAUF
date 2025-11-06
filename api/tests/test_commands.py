b647c0dfdb9f9f2168950bd9a4a53790861b048e
GsuDev <156043286+GsuDev@users.noreply.github.com>
2025-11-06 22:07:49 +0100

    HU5 Done

---
+++ api/tests/test_commands.py
@@ -0,0 +1,18 @@
+from django.core.management import call_command
+from django.test import TestCase
+from api.models import *
+
+
+class CommandsTests(TestCase):
+    def test_load_users_command(self):
+        # Nos aseguramos que no hay usuarios
+        self.assertEqual(User.objects.count(), 0)
+
+        # Ejecutamos el comando
+        call_command("load_users")
+
+        # Comprueba que se han creado los 30 usuarios
+        self.assertEqual(User.objects.count(), 30)
+
+        # Comprueba que ninguno tiene equipo
+        self.assertTrue(all(user.team is None for user in User.objects.all()))
